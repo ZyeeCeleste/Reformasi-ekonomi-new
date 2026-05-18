90f35c199bd497701c89db9b7b798ad4ced5d87e
Zyee <miyukifumeko@gmail.com>
2026-05-18 08:47:09 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,16 +1,3 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('log_errors', 1);
-error_reporting(E_ALL);
-
-// Log error ke file
-$logFile = '/tmp/php-error.log';
-ini_set('error_log', $logFile);
-
-try {
-    require __DIR__ . '/../public/index.php';
-} catch (Exception $e) {
-    error_log($e->getMessage());
-    echo "Error: " . $e->getMessage();
-}
-EOF
+// File ini bakal ngarahin semua request ke file index-nya Laravel.
+require __DIR__ . '/../public/index.php';
