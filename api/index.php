c5b99bb8c86a12c50349d86179bb2487e219fca7
Zyee <miyukifumeko@gmail.com>
2026-05-18 08:09:45 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,16 @@
 <?php
-require __DIR__ . '/../public/index.php';
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+error_reporting(E_ALL);
+
+// Log error ke file
+$logFile = '/tmp/php-error.log';
+ini_set('error_log', $logFile);
+
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (Exception $e) {
+    error_log($e->getMessage());
+    echo "Error: " . $e->getMessage();
+}
+EOF
